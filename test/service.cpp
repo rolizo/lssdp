db8d6dab5bb76c66562ba16240b90477a5b492db
Roland Zoufal <r.zoufal@commend.com>
2017-03-30 10:19:24 +0200

    barcode command line parameter

--- test/service.cpp
+++ test/service.cpp
@@ -11,42 +11,42 @@
 static volatile int keepRunning = 1;
 
 void intHandler(int dummy) {
-	keepRunning = 0;
+    keepRunning = 0;
 }
 
 
 void log_callback(const char * file, const char * tag, int level, int line,
                   const char * func, const char * message) {
-	std::string level_name = "DEBUG";
-	if (level == LSSDP_LOG_INFO)   level_name = "INFO";
-	if (level == LSSDP_LOG_WARN)   level_name = "WARN";
-	if (level == LSSDP_LOG_ERROR)  level_name = "ERROR";
+    std::string level_name = "DEBUG";
+    if (level == LSSDP_LOG_INFO)   level_name = "INFO";
+    if (level == LSSDP_LOG_WARN)   level_name = "WARN";
+    if (level == LSSDP_LOG_ERROR)  level_name = "ERROR";
 
-	printf("[%-5s][%s] %s", level_name.c_str(), tag, message);
+    printf("[%-5s][%s] %s", level_name.c_str(), tag, message);
 }
 
 long long get_current_time() {
-	struct timeval time = {};
-	if (gettimeofday(&time, NULL) == -1) {
-		printf("gettimeofday failed, errno = %s (%d)\n", strerror(errno), errno);
-		return -1;
-	}
-	return (long long) time.tv_sec * 1000 + (long long) time.tv_usec / 1000;
+    struct timeval time = {};
+    if (gettimeofday(&time, NULL) == -1) {
+        printf("gettimeofday failed, errno = %s (%d)\n", strerror(errno), errno);
+        return -1;
+    }
+    return (long long) time.tv_sec * 1000 + (long long) time.tv_usec / 1000;
 }
 
 
 
 void print_lssdp_packet(lssdp_packet &parsed_packet) {
 
-	printf("---------------------------------------------------\n");
-	printf("METHOD: %s\n",parsed_packet.method);
-	printf("ST: %s\n",parsed_packet.st);
-	printf("USN: %s\n",parsed_packet.usn);
-	printf("LOCATION: %s\n",parsed_packet.location);
-	printf("SM_ID: %s\n",parsed_packet.sm_id);
-	printf("DEVICE: %s\n",parsed_packet.device_type);
-	printf("NTS: %s\n",parsed_packet.nts);
-	printf("---------------------------------------------------\n");
+    printf("---------------------------------------------------\n");
+    printf("METHOD: %s\n",parsed_packet.method);
+    printf("ST: %s\n",parsed_packet.st);
+    printf("USN: %s\n",parsed_packet.usn);
+    printf("LOCATION: %s\n",parsed_packet.location);
+    printf("SM_ID: %s\n",parsed_packet.sm_id);
+    printf("DEVICE: %s\n",parsed_packet.device_type);
+    printf("NTS: %s\n",parsed_packet.nts);
+    printf("---------------------------------------------------\n");
 }
 
 
@@ -55,69 +55,74 @@ void print_lssdp_packet(lssdp_packet &parsed_packet) {
 
 int show_ssdp_packet(struct lssdp_ctx * lssdp, const char * packet,
                      size_t packet_len) {
-	printf("%s", packet);
+    printf("%s", packet);
 
 
 
-	lssdp_packet parsed_packet = {};
-	if (lssdp_packet_parser(packet, packet_len, &parsed_packet) == 0) {
+    lssdp_packet parsed_packet = {};
+    if (lssdp_packet_parser(packet, packet_len, &parsed_packet) == 0) {
 
-		print_lssdp_packet(parsed_packet);
-	}
+        print_lssdp_packet(parsed_packet);
+    }
 
 
-	return 0;
+    return 0;
 }
 
-int main() {
+int main( int argc, char *argv[] ) {
 
-	lssdp_ctx lssdp;
-	lssdp.port = 1900;
-	lssdp.debug = true;
-	lssdp.packet_received_callback = show_ssdp_packet;
+    lssdp_ctx lssdp;
+    lssdp.port = 1900;
+    lssdp.debug = true;
+    lssdp.packet_received_callback = show_ssdp_packet;
 
-	lssdp_init(&lssdp);
-	lssdp_set_log_callback(log_callback);
-	lssdp.config.ADDR_LOCALHOST = "::1";
-	lssdp.config.ADDR_MULTICAST = "FF02::C";
+    lssdp_init(&lssdp);
+    lssdp_set_log_callback(log_callback);
+    lssdp.config.ADDR_LOCALHOST = "::1";
+    lssdp.config.ADDR_MULTICAST = "FF02::C";
 
 
-	strncpy(lssdp.header.location.prefix,"http://\0",LSSDP_FIELD_LEN);
-	strncpy(lssdp.header.location.domain,"test_location",LSSDP_FIELD_LEN);
-	strncpy(lssdp.header.location.suffix,":8082\0",LSSDP_FIELD_LEN);
+    strncpy(lssdp.header.location.prefix,"http://\0",LSSDP_FIELD_LEN);
+    strncpy(lssdp.header.location.domain,"test_location",LSSDP_FIELD_LEN);
+    strncpy(lssdp.header.location.suffix,":8082\0",LSSDP_FIELD_LEN);
 
-	strncpy(lssdp.header.search_target,"commend_switchbox\0",LSSDP_FIELD_LEN);
-	strncpy(lssdp.header.device_type,"commend_switchbox\0",LSSDP_FIELD_LEN);
-	strncpy(lssdp.header.sm_id,"commend_switchbox\0",LSSDP_FIELD_LEN);
-	strncpy(lssdp.header.unique_service_name,"Barcode",LSSDP_FIELD_LEN);
-	lssdp_socket_create(&lssdp);
-	lssdp_send_notify(&lssdp);
+    strncpy(lssdp.header.search_target,"commend_switchbox\0",LSSDP_FIELD_LEN);
+    strncpy(lssdp.header.device_type,"commend_switchbox\0",LSSDP_FIELD_LEN);
+    strncpy(lssdp.header.sm_id,"commend_switchbox\0",LSSDP_FIELD_LEN);
+    if(argc == 2) {
+        strncpy(lssdp.header.unique_service_name,argv[1],LSSDP_FIELD_LEN);
+    } else {
+        strncpy(lssdp.header.unique_service_name,"DUMMY_BARCODE",LSSDP_FIELD_LEN);
 
-	//Capture CTRL-C and send bye before exiting
-	signal(SIGINT, intHandler);
+    }
+    lssdp_socket_create(&lssdp);
+    lssdp_send_notify(&lssdp);
 
-	// Main Loop
-	while (keepRunning) {
-		fd_set fs;
-		FD_ZERO(&fs);
-		FD_SET(lssdp.sock, &fs);
-		struct timeval tv;
-		tv.tv_sec = 0;
-		tv.tv_usec = 500 * 1000;   // 500 ms
+    //Capture CTRL-C and send bye before exiting
+    signal(SIGINT, intHandler);
 
-		int ret = select(lssdp.sock + 1, &fs, NULL, NULL, &tv);
-		if (ret < 0) {
-			printf("select error, ret = %d\n", ret);
-			break;
-		}
+    // Main Loop
+    while (keepRunning) {
+        fd_set fs;
+        FD_ZERO(&fs);
+        FD_SET(lssdp.sock, &fs);
+        struct timeval tv;
+        tv.tv_sec = 0;
+        tv.tv_usec = 500 * 1000;   // 500 ms
 
-		if (ret > 0) {
-			lssdp_socket_read(&lssdp);
-		}
+        int ret = select(lssdp.sock + 1, &fs, NULL, NULL, &tv);
+        if (ret < 0) {
+            printf("select error, ret = %d\n", ret);
+            break;
+        }
 
-	}
+        if (ret > 0) {
+            lssdp_socket_read(&lssdp);
+        }
 
-	lssdp_send_byebye(&lssdp);
+    }
 
-	return EXIT_SUCCESS;
+    lssdp_send_byebye(&lssdp);
+
+    return EXIT_SUCCESS;
 }
